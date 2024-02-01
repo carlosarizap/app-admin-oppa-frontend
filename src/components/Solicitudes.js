d121da8738df7e2252d4769ea25a1fccc681990c
Eduardo Fuentealba <eduardo.fuentealbau@gmail.com>
2024-02-01 16:49:28 -0300

    cambios en solicitud

--- src/components/Solicitudes.js
+++ src/components/Solicitudes.js
@@ -45,17 +45,25 @@ const Solicitudes = () => {
             const clienteResponse = await fetch(`${URL_BACKEND}/api/clientes/${solicitud.IdCliente}`).then((response) => response.json());
             const apoderadoResponse = await fetch(`${URL_BACKEND}/api/apoderados/${solicitud.IdApoderado}`).then((response) => response.json());
             
-            mapaClientes.set(solicitud.IdCliente, { id: solicitud._id, Rut: clienteResponse.Rut });
-            mapaClientes.set(solicitud.IdApoderado, { id: solicitud._id, Rut: apoderadoResponse.Rut });
+            if(clienteResponse != null && apoderadoResponse != null){
+              mapaClientes.set(solicitud.IdCliente, { id: solicitud._id, Rut: clienteResponse.Rut });
+              mapaClientes.set(solicitud.IdApoderado, { id: solicitud._id, Rut: apoderadoResponse.Rut });
+            }
+            
 
           }
-          else if(solicitud.IdCliente !== null){
+          else if(solicitud.IdCliente !== null && solicitud.IdApoderado  == null){
             const clienteResponse = await fetch(`${URL_BACKEND}/api/clientes/${solicitud.IdCliente}`).then((response) => response.json());
-            mapaClientes.set(solicitud.IdCliente, { id: solicitud._id, Rut: clienteResponse.Rut });
+            if(clienteResponse != null){
+              mapaClientes.set(solicitud.IdCliente, { id: solicitud._id, Rut: clienteResponse.Rut });
+            }
           }
-          else{
+          else if(solicitud.IdCliente == null && solicitud.IdApoderado  !== null){
             const apoderadoResponse = await fetch(`${URL_BACKEND}/api/apoderados/${solicitud.IdApoderado}`).then((response) => response.json());
-            mapaClientes.set(solicitud.IdApoderado, { id: solicitud._id, Rut: apoderadoResponse.Rut });
+            
+            if(apoderadoResponse != null){
+              mapaClientes.set(solicitud.IdApoderado, { id: solicitud._id, Rut: apoderadoResponse.Rut });
+            }
           }
         };
 
@@ -93,17 +101,25 @@ const Solicitudes = () => {
             const clienteResponse = await fetch(`${URL_BACKEND}/api/clientes/${solicitud.IdCliente}`).then((response) => response.json());
             const apoderadoResponse = await fetch(`${URL_BACKEND}/api/apoderados/${solicitud.IdApoderado}`).then((response) => response.json());
             
-            mapaClientes.set(solicitud.IdCliente, { id: solicitud._id, Rut: clienteResponse.Rut });
-            mapaClientes.set(solicitud.IdApoderado, { id: solicitud._id, Rut: apoderadoResponse.Rut });
+            if(clienteResponse != null && apoderadoResponse != null){
+              mapaClientes.set(solicitud.IdCliente, { id: solicitud._id, Rut: clienteResponse.Rut });
+              mapaClientes.set(solicitud.IdApoderado, { id: solicitud._id, Rut: apoderadoResponse.Rut });
+            }
+            
 
           }
-          else if(solicitud.IdCliente !== null){
+          else if(solicitud.IdCliente !== null && solicitud.IdApoderado  == null){
             const clienteResponse = await fetch(`${URL_BACKEND}/api/clientes/${solicitud.IdCliente}`).then((response) => response.json());
-            mapaClientes.set(solicitud.IdCliente, { id: solicitud._id, Rut: clienteResponse.Rut });
+            if(clienteResponse != null){
+              mapaClientes.set(solicitud.IdCliente, { id: solicitud._id, Rut: clienteResponse.Rut });
+            }
           }
-          else{
+          else if(solicitud.IdCliente == null && solicitud.IdApoderado  !== null){
             const apoderadoResponse = await fetch(`${URL_BACKEND}/api/apoderados/${solicitud.IdApoderado}`).then((response) => response.json());
-            mapaClientes.set(solicitud.IdApoderado, { id: solicitud._id, Rut: apoderadoResponse.Rut });
+            
+            if(apoderadoResponse != null){
+              mapaClientes.set(solicitud.IdApoderado, { id: solicitud._id, Rut: apoderadoResponse.Rut });
+            }
           }
         };
 
@@ -146,12 +162,12 @@ const Solicitudes = () => {
                     <tr key={solicitud._id} >
                         <td>{solicitud.NombreServicio}</td>
                         <td style={{ maxWidth: '200px', overflow: 'hidden', textOverflow: 'ellipsis', whiteSpace: 'nowrap' }}>
-                        {clienteMap.get(solicitud.IdCliente)?.Rut || 'N/A'}
+                          {solicitud.IdCliente !== null && clienteMap.has(solicitud.IdCliente) ? (clienteMap.get(solicitud.IdCliente)?.Rut || 'N/A') : 'N/A'}
                         </td>
                         <td style={{ maxWidth: '200px', overflow: 'hidden', textOverflow: 'ellipsis', whiteSpace: 'nowrap' }}>
-                        {clienteMap.get(solicitud.IdApoderado)?.Rut || 'N/A'}
-
+                          {solicitud.IdApoderado !== null && clienteMap.has(solicitud.IdApoderado) ? (clienteMap.get(solicitud.IdApoderado)?.Rut || 'N/A') : 'N/A'}
                         </td>
+
                         <td style={{ maxWidth: '200px', overflow: 'hidden', textOverflow: 'ellipsis', whiteSpace: 'nowrap' }}>
                             {solicitud.Region}
                         </td>
@@ -162,7 +178,7 @@ const Solicitudes = () => {
                             {format(new Date(solicitud.Fecha), 'yyyy-MM-dd', { locale: esLocale })}
                         </td>
                         <td style={{ maxWidth: '200px', overflow: 'hidden', textOverflow: 'ellipsis', whiteSpace: 'nowrap' }}>
-                            {solicitud.Hora}
+                            {solicitud.Hora.substring(0, 5)}
                         </td>
                         <td style={{
                             maxWidth: '200px',
